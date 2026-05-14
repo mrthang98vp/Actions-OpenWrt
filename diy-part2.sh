57bc5e9a0ca7af7e1ece8df5f691cfecbfad3b83
Hoang Thang <thangcamera98@gmail.com>
2026-05-14 11:49:02 +0700

    Modify device ID and enable Wi-Fi on boot
    Updated device identifier and enabled Wi-Fi by default.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-# 1. Sửa định danh thiết bị (Đúng target Xiaomi AX3000T)
-sed -i 's/Xiaomi Mi Router AX3000T/NR3053 Custom/g' target/linux/mediatek/dts/mt7981-xiaomi-mi-router-ax3000t.dts
-sed -i 's/ImmortalWrt/NR3053/g' package/base-files/files/bin/config_generate
-sed -i 's/Xiaomi Mi Router AX3000T/NR3053 Custom/g' package/base-files/files/bin/config_generate
 # 2. ÉP CÀI DRIVER WIFI (Full bộ cho chip MT7981)
 echo "CONFIG_PACKAGE_kmod-mt7981-firmware=y" >> .config
 echo "CONFIG_PACKAGE_kmod-mt79x1-common=y" >> .config
 echo "CONFIG_PACKAGE_kmod-mt7981-common=y" >> .config
 echo "CONFIG_PACKAGE_kmod-mt7981-wifi=y" >> .config
 echo "CONFIG_PACKAGE_wpad-mesh-openssl=y" >> .config
-# 3. Tự động bật Wifi ngay khi khởi động
+# Bật Wi-Fi mặc định (Mặc định OpenWrt sẽ tắt Wi-Fi, dòng này sẽ chuyển disabled từ 1 sang 0)
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 4. Giao diện & Tính năng
-echo "CONFIG_PACKAGE_luci-app-statistics=y" >> .config
+# Cấu hình IP mặc định là 192.168.1.1
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
